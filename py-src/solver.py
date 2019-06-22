41c2c463a415317cd04d304d840569a3e5a66693
Vladislav Shulepov <shulepov.vlad@gmail.com>
2019-06-22 16:42:45 +0300

    use more boosters

--- py-src/solver.py
+++ py-src/solver.py
@@ -1,8 +1,6 @@
-from constants import Booster
-from state import State, Cell
 import decode
 from encoder import Encoder
-from actions import MoveUp, MoveDown, MoveLeft, MoveRight, AttachManipulator
+from actions import *
 import pathfinder
 
 
@@ -37,31 +35,44 @@ def pathToCommands(path):
 LR = 1
 
 
+def attachCommand(bot):
+    global LR
+    turns = 0
+    while bot.manipulators[0] != (1, 0):
+        turns += 1
+        bot.turnLeft()
+    idx = 2
+    while not bot.is_attachable(1, idx * LR):
+        idx += 1
+    pos = (idx * LR, 1)
+    LR *= -1
+    while turns > 0:
+        turns -= 1
+        bot.turnRight()
+        pos = (pos[1], -pos[0])
+    return AttachManipulator(pos)
+
+
 def collectBoosters(st, bot):
     global LR
     while True:
         path = pathfinder.bfsFind(st, bot.pos,
-                                  lambda x, y: st.cell(x, y)[0] == Booster.MANIPULATOR)
+                                  lambda x, y: st.cell(x, y)[0] is not None)
         if path is None:
             break
         commands = pathToCommands(path)
         for command in commands:
             st.nextAction(command)
-
-        turns = 0
-        while bot.manipulators[0] != (1, 0):
-            turns += 1
-            bot.turnLeft()
-        idx = 2
-        while not bot.is_attachable(1, idx * LR):
-            idx += 1
-        pos = (idx * LR, 1)
-        LR *= -1
-        while turns > 0:
-            turns -= 1
-            bot.turnRight()
-            pos = (pos[1], -pos[0])
-        st.nextAction(AttachManipulator(pos))
+        if st.boosters[Booster.WHEEL] > 0 and bot.wheel_duration <= 0:
+            command = AttachWheels()
+        elif st.boosters[Booster.DRILL] > 0 and bot.drill_duration <= 0:
+            command = AttachDrill()
+        elif st.boosters[Booster.MANIPULATOR] > 0:
+            command = attachCommand(bot)
+        # TODO (all boosters)
+        else:
+            return
+        st.nextAction(command)
 
 
 def closestRotSolver(st):
