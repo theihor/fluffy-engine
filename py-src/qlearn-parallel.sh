19179b7df28a1ce8511b02983d5faefdc6fff60b
theihor <the.ihor@gmail.com>
2019-06-23 09:20:07 +0300

    venv

--- py-src/qlearn-parallel.sh
+++ py-src/qlearn-parallel.sh
@@ -1,9 +1,9 @@
-python q.py 1 100 &
-python q.py 50 150 &
-python q.py 100 200 &
-python q.py 200 250 &
-python q.py 250 300 &
-python q.py 225 275 
+./venv/bin/python q.py 250 260 &
+./venv/bin/python q.py 261 270 &
+./venv/bin/python q.py 271 280 &
+./venv/bin/python q.py 281 290 &
+./venv/bin/python q.py 291 300 &
+./venv/bin/python q.py 295 300 
 
 wait
 echo "All tasks complete!"
