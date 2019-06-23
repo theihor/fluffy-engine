4eaffd57fd1241419614b773e0e457794b7a1089
theihor <ihor.solodrai@gmail.com>
2019-06-23 12:22:53 +0300

    wip

--- py-src/generator.py
+++ py-src/generator.py
@@ -41,26 +41,25 @@ def generate(c):
     # draw a line to frame from every 'obstacle'
     def fill_to_frame(x, y, dx=0, dy=0):
         if dx != 0:
-            while 0 <= x < size - 1:
+            while 0 <= x < size - 1 and cells[y][x][1] == Cell.ROT:
                 #print("cells[" + str(y) + "][" + str(x) + "]")
                 cells[y][x] = o
                 x += dx
         elif dy != 0:
-            while 0 <= y < size - 1:
+            while 0 <= y < size - 1 and cells[y][x][1] == Cell.ROT:
                 cells[y][x] = o
                 y += dy
 
-    for (x, y) in c['oSqs']:
-
+    def draw_from_o(x, y):
         # trying go right
         can_go = True
-        for dx in range(size-x-1):
+        for dx in range(size - x - 1):
             if (x + dx, y) in c['iSqs']:
                 can_go = False
                 break
         if can_go:
             print("fillin from " + str((x, y)))
-            fill_to_frame(x, y, dx = 1)
+            fill_to_frame(x, y, dx=1)
         else:
             can_go = True
             # try left
@@ -68,7 +67,8 @@ def generate(c):
                 if (x - dx, y) in c['iSqs']:
                     can_go = False
                     break
-            if can_go: fill_to_frame(x, y, dx=-1)
+            if can_go:
+                fill_to_frame(x, y, dx=-1)
             else:
                 can_go = True
                 # try up
@@ -84,9 +84,103 @@ def generate(c):
                         if (x, y - dy) in c['iSqs']:
                             can_go = False
                             break
-                    if can_go: fill_to_frame(x, y, dy=-1)
-                    else: raise RuntimeError("Need maneur :(")
+                    if can_go:
+                        fill_to_frame(x, y, dy=-1)
+                    else:
+                        raise RuntimeError("Need maneur :(")
+
+    # print_cells(cells, size, size)
+
+    for (x, y) in c['oSqs']: draw_from_o(x, y)
+        # # trying go right
+        # can_go = True
+        # for dx in range(size-x-1):
+        #     if (x + dx, y) in c['iSqs']:
+        #         can_go = False
+        #         break
+        # if can_go:
+        #     print("fillin from " + str((x, y)))
+        #     fill_to_frame(x, y, dx = 1)
+        # else:
+        #     can_go = True
+        #     # try left
+        #     for dx in range(x):
+        #         if (x - dx, y) in c['iSqs']:
+        #             can_go = False
+        #             break
+        #     if can_go: fill_to_frame(x, y, dx=-1)
+        #     else:
+        #         can_go = True
+        #         # try up
+        #         for dy in range(size - y - 1):
+        #             if (x, y + dy) in c['iSqs']:
+        #                 can_go = False
+        #                 break
+        #             if can_go: fill_to_frame(x, y, dy=1)
+        #         else:
+        #             can_go = True
+        #             # try down
+        #             for dy in range(y):
+        #                 if (x, y - dy) in c['iSqs']:
+        #                     can_go = False
+        #                     break
+        #             if can_go: fill_to_frame(x, y, dy=-1)
+        #             else: raise RuntimeError("Need maneur :(")
     #print_cells(cells, size, size)
+
+    coords = cells_to_polygon(cells, size, size)
+
+
+    while len(coords) < c['vMin']:
+        print("Not enough vertices: " + str(len(coords)))
+        n = round(c['vMin'] - len(coords) / 4)
+        i = 0
+        while i < len(coords) - 1:
+            (x1, y1) = coords[i]
+            (x2, y2) = coords[i+1]
+
+            if x1 == x2 and abs(y2 - y1) >= 3:
+                if y1 < y2: # up
+                    allowed_to_fill = True
+                    for y in range(y1, y1 + 3):
+                        for x in range(x1 - 2, x1):
+                            if (x, y) in c['iSqs'] or cells[y][x][1] == Cell.OBSTACLE:
+                                allowed_to_fill = False
+                                break
+                    if allowed_to_fill:
+                        cells[y1 + 1][x1 - 1] = o
+                else: # down
+                    allowed_to_fill = True
+                    for y in range(y2, y2 + 3):
+                        for x in range(x1 - 2, x1):
+                            if (x, y) in c['iSqs'] or cells[y][x][1] == Cell.OBSTACLE:
+                                allowed_to_fill = False
+                                break
+                    if allowed_to_fill:
+                        cells[y2 + 1][x1 + 1] = o
+            elif y1 == y2 and abs(x2 - x1) >= 3:
+                if x1 < x2: # right
+                    allowed_to_fill = True
+                    for x in range(x1, x1 + 3):
+                        for y in range(y1, y1 + 2):
+                            if (x, y) in c['iSqs'] or cells[y][x][1] == Cell.OBSTACLE:
+                                allowed_to_fill = False
+                                break
+                    if allowed_to_fill:
+                        cells[y1 + 1][x1 + 1] = o
+                # else: # down
+                #     allowed_to_fill = True
+                #     for y in range(y2, y2 + 3):
+                #         for x in range(x1 - 2, x1):
+                #             if (x, y) in c['iSqs'] or cells[y][x][1] == Cell.OBSTACLE:
+                #                 allowed_to_fill = False
+                #                 break
+                #     if allowed_to_fill:
+                #         cells[y2 + 1][x1 - 1] = o
+            i += 1
+
+        coords = cells_to_polygon(cells, size, size)
+
     if c['iSqs']:
         start_pos = random.choice(c['iSqs'])
     else: start_pos = (0, 0)
@@ -139,15 +233,15 @@ def cells_to_polygon(cells, width, height):
             raise RuntimeError("SHould never be here")
 
     while True:
-        print(str(x) + " " + str(y))
+        #print(str(x) + " " + str(y))
         # check if obstacle is right from us
         ((dx, dy), new_face) = move(Direction.RIGHT)
         if is_o(dx, dy):
-            print("obstacle right from us, check forward")
+            #print("obstacle right from us, check forward")
 
             ((dx, dy), new_face) = move(Direction.UP)
             if is_o(dx, dy):
-                print( "obstacle in front of us and right from us, turn left")
+                #print( "obstacle in front of us and right from us, turn left")
                 (_, new_face) = move(Direction.LEFT)
                 if new_face != face:
                     if face == Direction.RIGHT:
@@ -163,12 +257,12 @@ def cells_to_polygon(cells, width, height):
                     raise RuntimeError("This should never happen")
                 pass
             else:
-                print(" no obstacle in front, go forward")
+                #print(" no obstacle in front, go forward")
                 x += dx; y += dy
                 moved = True
 
         else:
-            print("no obstacle right from us, turn right and go there")
+            #print("no obstacle right from us, turn right and go there")
             # no obstacle right from us,
             # turn right and go there
             if new_face != face:
@@ -199,6 +293,6 @@ def generate_and_dump(puzzle):
     coords = cells_to_polygon(cells, size, size)
     encode_generated_map("../map.desc", coords, start_pos)
 
-p = parse_puzzle("../examples/puzzle.cond")
+p = parse_puzzle("../puzzles/puzzle20-56.cond")
 generate_and_dump(constraints_from_parsed(p))
 
